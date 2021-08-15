0920b9012fe6029d7ce92e8900bc960ff4696b3f
arafatanjan <arafatanjan@gmail.com>
2021-08-15 16:32:36 +0600

    first commit

--- js/bnk.js
+++ js/bnk.js
@@ -1,1 +1,13 @@
-document.getElementById('login-submit')
+document.getElementById('login-submit').addEventListener('click', function () {
+
+    const emilFeild = document.getElementById('user-emil');
+    const userEmil = emilFeild.value;
+    // usser pssword
+    const psswordField = document.getElementById('user-pssword');
+    const userPssword = psswordField.value;
+
+    if (userEmil == 'wwwww@gmil.com' && userPssword == 'secret') {
+        window.location.href = 'bnking.html';
+    }
+
+})
